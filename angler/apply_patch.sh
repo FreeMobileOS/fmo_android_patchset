5aa07fc3fb5ff870b72ea3e066cf7423d18d02e4
Bernhard Rosenkränzer <bero@lindev.ch>
2019-12-26 01:57:03 +0100

    Fix mixed tab/space indentation

--- angler/apply_patch.sh
+++ angler/apply_patch.sh
@@ -8,7 +8,7 @@ if [ -z "$AOSP_SRC_PATH" ]; then
 	echo "Enter AOSP src path:"
 	read srcpath
 	AOSP_SRC_PATH=$srcpath
-    export AOSP_SRC_PATH=$AOSP_SRC_PATH
+	export AOSP_SRC_PATH=$AOSP_SRC_PATH
 fi
 
 echo "AOSP Source: $AOSP_SRC_PATH"
@@ -21,20 +21,18 @@ for dir in "${dirs[@]}"; do
 	PATCH_DIR=$(pwd)
 	files=($(find . -type f -name '*.patch' | sort -n))
 	count=($(find . -type f -name '*.patch' | wc -l ))
-    if [ $count -gt 0 ]; then
-        cd $AOSP_SRC_PATH/$dir
-        for file in "${files[@]}"; do
-            echo ""
-            echo "applying patch@: $AOSP_SRC_PATH/$dir"
-            echo "which patch: $file"
-		    git am --signoff $PATCH_DIR/$file >> $PATCH_DIR/log 2>&1
-		    if [ $? -eq 128 ]; then
-			    echo "Patch Failed. Lookup $PATCH_DIR/log for more info. Aborting..."
-			    git am --abort
-		    fi
+	if [ $count -gt 0 ]; then
+		cd $AOSP_SRC_PATH/$dir
+		for file in "${files[@]}"; do
+			echo ""
+			echo "applying patch@: $AOSP_SRC_PATH/$dir"
+			echo "which patch: $file"
+			git am --signoff $PATCH_DIR/$file >> $PATCH_DIR/log 2>&1
+			if [ $? -eq 128 ]; then
+				echo "Patch Failed. Lookup $PATCH_DIR/log for more info. Aborting..."
+				git am --abort
+			fi
 		done
 	fi
 	cd $MYDIR 
 done
-
-
